8e8eb526bfa48b22cc597b3cbd945ea129ae7109
Benjamin du Preez <benjamindupreez@gmail.com>
2018-04-16 16:16:34 +0200

    commiting latest changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+import {InAppBrowser} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: string = "";
+
+  constructor(private barcodeScanner: BarcodeScanner, private iab: InAppBrowser) {
+
 
   }
 
+  openQRScanner() {
+    this.barcodeScanner.scan().then(barcodeData => {
+
+      // get h2c url from scanned barcode
+      this.url = barcodeData.text;
+
+      // go to the url on h2c
+      this.iab.create(this.url, '_system');
+
+      console.log('Barcode data', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 }
